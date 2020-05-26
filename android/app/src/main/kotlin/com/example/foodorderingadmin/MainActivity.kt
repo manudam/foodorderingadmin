9ee438fbc2f161115a5275789d2ae75528ea709a
Manuel Dambrine <manuel.dambrine@trianglecarrot.com>
2020-05-26 11:18:10 +0100

    first checkin

---
+++ android/app/src/main/kotlin/com/example/foodorderingadmin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.foodorderingadmin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/menu_admin_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MenuAdminScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
